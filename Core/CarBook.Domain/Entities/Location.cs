6a14137234a4280aa678b7bc2106a2f9d172219b
celalcolak94 <celalcolak94@gmail.com>
2024-02-10 22:18:53 +0300

    Add project files.

---
+++ Core/CarBook.Domain/Entities/Location.cs
@@ -0,0 +1,8 @@
+﻿namespace CarBook.Domain.Entities
+{
+    public class Location
+    {
+        public int LocationID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Core/CarBook.Domain/Entities/SocialMedia.cs
@@ -0,0 +1,10 @@
+﻿namespace CarBook.Domain.Entities
+{
+    public class SocialMedia
+    {
+        public int SocialMediaID { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Icon { get; set; }
+    }
+}
